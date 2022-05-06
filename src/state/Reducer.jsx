8989d7c271e75df0445526583eae0a99cb8f4a0e
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-06 11:23:42 -0500

    Reducer function created v1.0
    Dummy reducer function created for testing

---
+++ src/state/Reducer.jsx
@@ -0,0 +1,16 @@
+function reducer(state, action){
+    switch(action.type){
+        case 'add-category':
+            return state
+        case 'remove-category':
+            return state
+        case 'get-category':
+            return state
+        case 'add-todo':
+            return state
+        case 'remove-todo':
+            return state
+        case 'update-todo':
+            return state
+    }
+}
